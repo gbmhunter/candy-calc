3f481af3e25ff6e1b0dc63a5cda0c27f98a6ee53
Geoffrey Hunter <gbmhunter@gmail.com>
2014-02-17 17:20:04 +1300

    'Basic Multiplication Test' is now implemented correctly.

--- test/QUnitTests.js
+++ test/QUnitTests.js
@@ -1,4 +1,14 @@
-test( "hello test", function() {
-	console.log('QUnit test ran!');
-  ok( 1 == "1", "Passed!" );
-});
+window.onload=function(){
+	test("Basic Multiplication Test", function() {
+		
+		jQuery("#voltage").val(1).change();
+		jQuery("#current").val(1).change();
+		
+		stop();
+		
+		setTimeout(function(){
+			ok(jQuery("#resistance").val() == "1", "Passed!" );
+			start();
+		}, 1000); 
+	});
+};
