66e0de618f049f4ee7205dc427dfb11250f45eac
Geoffrey Hunter <gbmhunter@gmail.com>
2014-02-17 15:54:45 +1300

    Added QUnit tests.

---
+++ test/QUnitTests.js
@@ -0,0 +1,4 @@
+test( "hello test", function() {
+	console.log('QUnit test ran!');
+  ok( 1 == "1", "Passed!" );
+});
--- test/test.php
+++ test/test.php
@@ -6,10 +6,10 @@
   <!--<link rel="stylesheet" href="http://code.jquery.com/qunit/qunit-1.14.0.js">-->
 </head>
 <body>
-  <!--<div id="qunit"></div>
+  <div id="qunit"></div>
   <div id="qunit-fixture"></div>
   <script src="http://code.jquery.com/qunit/qunit-1.14.0.js"></script>
-  <script src="tests.js"></script>-->
+  <script src="QUnitTests.js"></script>
   <p>Testing!</p>
 </body>
 </html>
